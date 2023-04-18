c28b38e791818d0bb5a15e7beba2e4e9b7636ea4
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2023-04-18 15:28:55 +0200

    Add files via upload

---
+++ login/spe-select/spe-select.php
@@ -0,0 +1,121 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Choisir Spécialitées</title>
+  
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel="stylesheet" href="./spe-select.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<form method="POST" action="../../php-shit/php-files/speSelector.php">
+	<fieldset class="checkbox-group">
+		<legend class="checkbox-group-legend">Choisi tes Spécialités</legend>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" name="spe" class="checkbox-input" value="ses" id="ses-checkbox" />
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">Ses</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" name="spe-select" value="maths" id="maths-ses"/>
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">Maths</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" />
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">Phisique Chimie</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" name="hlp"/>
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">HLP</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" name="nsi" />
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">NSI</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" name="geopo"/>
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">Géopolitique</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" />
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">SVT</span>
+				</span>
+			</label>
+		</div>
+		<div class="checkbox">
+			<label class="checkbox-wrapper">
+				<input type="checkbox" class="checkbox-input" name="anglais"/>
+				<span class="checkbox-tile">
+					<span class="checkbox-icon">
+						<!-- -->
+					</span>
+					<span class="checkbox-label">Anglais</span>
+				</span>
+			</label>  
+		</div>
+	</fieldset>
+</form>
+<div class="submit">           <!-- Redirect to control panel -->
+	<button id="button-submit" class="submit-btn">Suivant</button>
+    <script type="text/javascript">
+      document.getElementById("button-submit").onclick = function () {
+          location.href = "../../content/loader/loading.html";
+      };
+    </script>
+</div>
+
+<!-- partial -->
+  <script  src="./spe-select.js"></script>
+
+</body>
+</html>
