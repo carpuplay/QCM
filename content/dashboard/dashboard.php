067b156bbc05aa4f8469a77fe0a41b7dba5d891c
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2023-06-01 09:03:26 +0200

    #92 merge fix

--- content/dashboard/dashboard.php
+++ content/dashboard/dashboard.php
@@ -2,8 +2,9 @@
 session_start();
 include_once("../../php-shit/php-files/config.php");
 
-// Fetch distinct subjects from the chapitres table
-$query = "SELECT DISTINCT spe FROM chapitres";
+// Fetch distinct subjects from the questions table
+$query = "SELECT DISTINCT spe
+          FROM questions";
 $result = mysqli_query($link, $query);
 
 $subjects = array();
@@ -11,118 +12,151 @@ $subjects = array();
 if ($result && mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $subject = $row['spe'];
+        $subjects[] = $subject;
+    }
+}
 
-        // Fetch the chapters for the current subject
-        $chapterQuery = "SELECT DISTINCT chapitre FROM chapitres WHERE spe = '$subject'";
-        $chapterResult = mysqli_query($link, $chapterQuery);
+// Fetch distinct levels from the questions table
+$query = "SELECT DISTINCT niveaux 
+          FROM questions";
+$result = mysqli_query($link, $query);
 
-        $chapters = array();
-        if ($chapterResult && mysqli_num_rows($chapterResult) > 0) {
-            while ($chapterRow = mysqli_fetch_assoc($chapterResult)) {
-                $chapter = $chapterRow['chapitre'];
-                $chapters[] = $chapter;
-            }
-        }
+$levels = array();
 
-        $subjects[$subject] = $chapters;
+if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $level = $row['niveaux'];
+        $levels[] = $level;
+    }
+}
+
+// Fetch distinct chapters from the questions table based on the question IDs in the qcm table
+$query = "SELECT DISTINCT chapitre 
+          FROM questions
+          INNER JOIN qcm ON FIND_IN_SET(questions.id, qcm.`questionId`)";
+$result = mysqli_query($link, $query);
+
+$chapters = array();
+
+if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $chapter = $row['chapitre'];
+        $chapters[] = $chapter;
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Quiz Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        $(document).ready(function() {
-            $('#subject').change(function() {
-                var subject = $(this).val();
+        $(document).ready(function () {
+            // Load the chapters based on the selected subject and level
+            $("#subject, #level").change(function () {
+                var subject = $("#subject").val();
+                var level = $("#level").val();
 
-                if (subject !== '') {
-                    $.ajax({
-                        url: 'get_chapters.php',
-                        type: 'post',
-                        data: {
-                            subject: subject
-                        },
-                        dataType: 'json',
-                        success: function(response) {
-                            $('#chapter').empty().append('<option value="">Select a chapter</option>');
-
-                            if (response.length > 0) {
-                                $.each(response, function(index, chapter) {
-                                    $('#chapter').append('<option value="' + chapter + '">' + chapter + '</option>');
-                                });
-                            }
-                        }
-                    });
-                } else {
-                    $('#chapter').empty().append('<option value="">Select a chapter</option>');
-                }
-            });
-
-            $('#level').change(function() {
-                var subject = $('#subject').val();
-                var level = $(this).val();
-
-                if (subject !== '' && level !== '') {
-                    $.ajax({
-                        url: 'get_chapters.php',
-                        type: 'post',
-                        data: {
-                            subject: subject,
-                            level: level
-                        },
-                        dataType: 'json',
-                        success: function(response) {
-                            $('#chapter').empty().append('<option value="">Select a chapter</option>');
-
-                            if (response.length > 0) {
-                                $.each(response, function(index, chapter) {
-                                    $('#chapter').append('<option value="' + chapter + '">' + chapter + '</option>');
-                                });
-                            }
-                        }
-                    });
-                } else {
-                    $('#chapter').empty().append('<option value="">Select a chapter</option>');
-                }
+                $.ajax({
+                    url: "get_chapters.php",
+                    method: "POST",
+                    data: {subject: subject, level: level},
+                    dataType: "json",
+                    success: function (data) {
+                        $("#chapter").empty();
+                        $("#chapter").append('<option value="">All Chapters</option>');
+                        $.each(data, function (index, chapter) {
+                            $("#chapter").append('<option value="' + chapter + '">' + chapter + '</option>');
+                        });
+                    }
+                });
             });
         });
-
     </script>
 </head>
 <body>
-    <h1>Quiz Dashboard</h1>
-    <form action="../quiz_interface/quiz.php" method="post">
+    <div class="title-1">
+        <h1>Quiz Dashboard</h1>
+    </div>
+    <form action="" method="post">
         <label for="subject">Subject:</label>
         <select name="subject" id="subject">
-            <option value="">Select a subject</option>
+            <option value="">All Subjects</option>
             <?php
-            $query = "SELECT DISTINCT spe FROM chapitres";
-            $result = mysqli_query($link, $query);
-
-            if ($result && mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $subject = $row['spe'];
-                    echo '<option value="' . $subject . '">' . $subject . '</option>';
-                }
+            foreach ($subjects as $subject) {
+                $selected = ($_POST['subject'] ?? '') === $subject ? 'selected' : '';
+                echo '<option value="' . $subject . '" ' . $selected . '>' . $subject . '</option>';
             }
             ?>
         </select>
 
         <label for="level">Level:</label>
         <select name="level" id="level">
-            <option value="">Select a level</option>
-            <option value="premiere">Premiere</option>
-            <option value="terminale">Terminale</option>
+            <option value="">All Levels</option>
+            <?php
+            foreach ($levels as $level) {
+                $selected = ($_POST['level'] ?? '') === $level ? 'selected' : '';
+                echo '<option value="' . $level . '" ' . $selected . '>' . $level . '</option>';
+            }
+            ?>
         </select>
 
         <label for="chapter">Chapter:</label>
         <select name="chapter" id="chapter">
-            <option value="">Select a chapter</option>
+            <option value="">All Chapters</option>
+            <?php
+            foreach ($chapters as $chapter) {
+                $selected = ($_POST['chapter'] ?? '') === $chapter ? 'selected' : '';
+                echo '<option value="' . $chapter . '" ' . $selected . '>' . $chapter . '</option>';
+            }
+            ?>
         </select>
-        <input type="submit" value="Start Quiz">
+
+        <input type="submit" value="Apply Filters">
     </form>
+
+    <?php
+    // Check if the filters form is submitted
+    if (isset($_POST['subject']) || isset($_POST['level']) || isset($_POST['chapter'])) {
+        $selectedSubject = $_POST['subject'] ?? '';
+        $selectedLevel = $_POST['level'] ?? '';
+        $selectedChapter = $_POST['chapter'] ?? '';
+
+        $query = "SELECT qcm.titre 
+                  FROM qcm
+                  INNER JOIN questions ON FIND_IN_SET(questions.id, qcm.`questionId`)";
+
+        // Apply the filters to the query
+        $conditions = array();
+
+        if (!empty($selectedSubject)) {
+            $conditions[] = "questions.spe = '$selectedSubject'";
+        }
+
+        if (!empty($selectedLevel)) {
+            $conditions[] = "questions.niveaux = '$selectedLevel'";
+        }
+
+        if (!empty($selectedChapter)) {
+            $conditions[] = "questions.chapitre = '$selectedChapter'";
+        }
+
+        if (!empty($conditions)) {
+            $query .= " WHERE " . implode(" AND ", $conditions);
+        }
+
+        $result = mysqli_query($link, $query);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<p>" . $row['titre'] . "</p>";
+            }
+        } else {
+            echo "<p>No results found.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
